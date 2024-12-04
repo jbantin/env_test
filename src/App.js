91ba55242e2528b3d2630f4b22e3c458673c1305
jbantin <jbantin@gmx.de>
2024-12-04 18:32:25 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi, I am {process.env.REACT_APP_NAME}</h1>
+      <h1>I work in {process.env.REACT_APP_COMPANY}</h1>
+      <h2>
+        Here we are hosting this website on
+        {process.env.REACT_APP_HOSTING}
+      </h2>
     </div>
   );
 }
